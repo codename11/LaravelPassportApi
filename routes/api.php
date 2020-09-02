851e73c50bc20ab2aff207c9f7e38f5451991bf2
Veljko <veljkos82@gmail.com>
2020-09-02 05:16:22 +0200

    Added cors.

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
 
-Route::apiResource('/post', 'Api\PostsController')->middleware('auth:api');
+Route::apiResource('/post', 'Api\PostsController')->middleware('auth:api', "cors");
